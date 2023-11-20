66b22fc670f2a7dc426803c3f0abf2bb9b635c50
robbfox <foxy@lovecat.com>
2023-11-20 10:23:36 +0000

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import MainContainer from "./MainContainer"
+// index.js or App.js
+import './bulma/css/bulma.min.css';
+
 
 function App() {
   return <MainContainer />
--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -1,12 +1,20 @@
 import { OrbitControls  } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber"
 import AnimatedStars from "./AnimatedStars";
+import { Html } from "@react-three/drei";
+
 
 const MainContainer = () => {
     return <Canvas>
         <color attach="background" args={["#301934"]} />
         <AnimatedStars />
         <OrbitControls />
+        <Html position={[-0.6, 0, 0]}>
+        <button class="button is-success is-large is-hovered has-text-success" style = {{backgroundColor: 'transparent', borderTopColor: '#FF6FFF', borderLeftColor: '#FF00FF', borderBottomColor: '#FF00FF', borderRightColor: '#FF6FFF'}}   onClick={() => console.log("Button clicked!")}>
+  Click me
+</button>
+
+  </Html>
     </Canvas>
 }
  export default MainContainer;
